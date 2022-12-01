da068a19084836bdf5d5d663d27cb8d968d4aa14
Denis Romodanov <darvenommm@gmail.com>
2022-12-01 18:21:39 +0300

    Дорабатывает компонент navigation

--- source/js/map.js
+++ source/js/map.js
@@ -1,7 +1,9 @@
+'use strict';
+
 const center = [59.938635, 30.323118];
 
 ymaps.ready(() => {
-  const map = new ymaps.Map("map", {
+  const map = new ymaps.Map('map', {
     center: center,
     zoom: 17,
     controls: [],
@@ -10,12 +12,12 @@ ymaps.ready(() => {
   const placemark = new ymaps.Placemark(
     center,
     {
-      balloonContentHeader: "Адрес",
-      balloonContentBody: "г. Санкт-Петербург ул. Большая Конюшенная д. 19/8, офис 101",
+      balloonContentHeader: 'Адрес',
+      balloonContentBody: 'г. Санкт-Петербург ул. Большая Конюшенная д. 19/8, офис 101',
     },
     {
-      iconLayout: "default#image",
-      iconImageHref: "./img/icons/map-icon.svg",
+      iconLayout: 'default#image',
+      iconImageHref: './img/icons/map-icon.svg',
       iconImageSize: [67, 100],
       iconImageOffset: [-50, -90],
     }
