6a64dc6a6fa47e4925facefecb004253f957fcd4
Denis Romodanov <darvenommm@gmail.com>
2022-12-20 18:57:33 +0300

    Переименовывает переменную в gulp settings

--- gulp/tasks/development/copySvg.js
+++ gulp/tasks/development/copySvg.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import settings from './../../settings.js';
 
 const copySvg = () => {
-  return gulp.src([`${settings.src}/img/**/*.svg`, `!${settings.sprite}/**/*.svg`])
+  return gulp.src([`${settings.src}/img/**/*.svg`, `!${settings.spriteFiles}`])
     .pipe(gulp.dest(`${settings.dest}/img`));
 };
 
--- gulp/tasks/production/optimizeImages.js
+++ gulp/tasks/production/optimizeImages.js
@@ -3,7 +3,7 @@ import imagemin from 'gulp-imagemin';
 import settings from './../../settings.js';
 
 const optimizeImages = () => {
-  return gulp.src([`${settings.src}/img/**/*.{jpg,jpeg,png,svg,gif}`, `!${settings.sprite}/**/*.svg`])
+  return gulp.src([`${settings.src}/img/**/*.{jpg,jpeg,png,svg,gif}`, `!${settings.spriteFiles}`])
     .pipe(imagemin())
     .pipe(gulp.dest(`${settings.dest}/img`));
 };
